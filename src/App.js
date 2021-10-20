5fec21fe3bcebe2923017921a440e8df85c7fff8
Noah Smith <noahsmittyy@gmail.com>
2021-10-20 14:29:55 -0700

    finished firebase connection - all works

--- src/App.js
+++ src/App.js
@@ -4,60 +4,63 @@ import {useState} from "react";
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 import AddTask from "./AddTask";
 import Alert from "./Alert";
+import firebase from "firebase/compat";
+import {useCollection} from "react-firebase-hooks/firestore";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcjlwvBtxAl8BKu_8evho99ks_6MBiWZo",
+    authDomain: "nskr-124lab4.firebaseapp.com",
+    projectId: "nskr-124lab4",
+    storageBucket: "nskr-124lab4.appspot.com",
+    messagingSenderId: "613050589081",
+    appId: "1:613050589081:web:d234d16f271e4ebe1ffd08",
+    measurementId: "G-6XSHMJM1HG"
+};
 
-const initialData = [
-    {
-        id: '1',
-        description: "Text John about Bank Statements",
-        isCompleted: false,
-    },
-    {
-        id: '2',
-        description: "Eat Lunch",
-        isCompleted: false,
-    },
-    {
-        id: '3',
-        description: "Call Mom",
-        isCompleted: true,
-    },
-    {
-        id: '4',
-        description: "Buy new John Grisham book",
-        isCompleted: false,
-    }
-];
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
+const collectionName = "List";
 function App(props) {
-    const [data, setData] = useState(initialData);
+    const query = db.collection(collectionName);
+    const [value, loading, error] = useCollection(query);
     const [isVisible, setVisibility] = useState(true);
     const [showAlert, setShowAlert] = useState(false);
     const [storeID, setStoreID] = useState("");
 
+    let data = null;
+    if (value) {
+        data = value.docs.map((doc) => {
+            return {...doc.data()}});
+    }
     function addData(description) {
-        setData([...data, {
+        const item = {
             id: generateUniqueID(),
             description: description,
             isCompleted: false,
-        }])
+        };
+        const docRef = query.doc(item.id);
+        docRef.set(item);
     }
 
     function handleItemChange(itemID, field, value) {
-        setData(data.map(item => item.id !== itemID ? item :
-            {...item, [field]: value}
-        ))
+        const doc = db.collection(collectionName).doc(itemID);
+        doc.update({
+            [field]: value,
+        })
     }
 
     function handleEditItem(field, value) {
-        console.log(storeID);
-        setData(data.map(item => item.id !== storeID ? item :
-            {...item, [field]: value}
-        ))
+        const doc = db.collection(collectionName).doc(storeID);
+        doc.update({
+            [field]: value,
+        })
     }
 
-    function handleDelete() {
-        setData(data.filter(item => !(item.isCompleted)));
+    function handleDelete(itemID) {
+        data.forEach((item) => item.isCompleted && db.collection(collectionName).doc(item.id).delete());
     }
 
     function toggleModal() {
@@ -65,7 +68,6 @@ function App(props) {
     }
 
     function changeID(itemID) {
-        console.log("changeID called", itemID);
         setStoreID(itemID);
     }
 
@@ -73,14 +75,16 @@ function App(props) {
         <div className={"todo"}>
             <div>
                 <h1>TO-DO LIST</h1>
+
                 <div className={isVisible ? "visible" : null}>
                     <button className={"button"} type={"button"} onClick={() => {
                         setVisibility(!isVisible);
                     }}>{isVisible ? "Hide Completed" : "Show Completed"}</button>
                     <button className={"button"} type={"button"} onClick={handleDelete}>Delete Completed</button>
                 </div>
-                <List todo={isVisible ? data : data.filter(item => !(item.isCompleted))} onItemChange={handleItemChange}
-                      onButtonClick={toggleModal} passID={changeID}/>
+
+                {data && <List todo={isVisible ? data : data.filter(item => !(item.isCompleted))}
+                      onItemChange={handleItemChange} onButtonClick={toggleModal} passID={changeID}></List>}
                 <AddTask data={data} onSubmit={addData}/>
             </div>
             {showAlert && <Alert onClose={toggleModal} onOK={handleEditItem}/>}
